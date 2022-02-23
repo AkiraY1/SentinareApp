3d42b5e78cb019efaeb4e1bc61edaea5dfd034a7
Akira Yoshiyama <luckyanchor2@gmail.com>
2022-02-23 01:34:55 -0800

    minimum case for app, added streamlit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import requests, time
+import requests, time, datetime, threading
+import streamlit as st
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 id = "tssOCgWHeoFCF6ym"
 secret = "IWI7gfUGRmYvJy83VXZhnik0Dsy6HinMC6GQ2bpawZR7te6GLW8IvBEDsiJPAFZ1"
 access_token = "aa475216f6aae2b0ec911e75d2d28e4d7579ada1"
-#Expires in 60 days
-
 base = "https://api.altumview.ca/v1.0/"
 baseOAuth = "https://canada-1.oauth.altumview.com/v1.0/token"
+
+BUFF = 1800 #30 minutes
 
 def getAccessToken():
     params = {"grant_type": "client_credentials", "client_id": id, "client_secret": secret, "scope": "room:write room:read camera:write camera:read alert:read alert:write person:write person:read"} #Gets client credentials
@@ -20,16 +24,69 @@ def getVisits():
     response = requests.get(url, headers=headers)
     return response.json()["data"]["visits"]["array"]
 
-adj_list = {}
+visits = {}
+def logVisits():
+    for visit in getVisits():
+        dep_time = visit["departure_time"]
+        id = visit["person"]["id"] #Gets ID instead of name in case multiple people have the same name
+        if id in visits:
+            visits[id].append(dep_time)
+        else:
+            visits[id] = [dep_time]
+    print(visits)
 
-#Schedule to check visits
-for visit in getVisits():
-    dep_time = visit["departure_time"]
-    id = visit["person"]["id"] #Gets ID instead of name in case multiple people have the same name
-    if len(adj_list) != 0:
-        for person in adj_list:
-            for time in person:
-                if abs(time[0]-dep_time) < 900: #If time difference between two people is less than 900 seconds (15 mins)
-                    adj_list[person].append((id, dep_time))
+def makeGraph(fro, to):
+    df = pd.DataFrame({ 'from':fro, 'to':to})
+    G = nx.from_pandas_edgelist(df, 'from', 'to')
+    nx.draw(G, with_labels=True, node_size=1500, node_color="skyblue", pos=nx.fruchterman_reingold_layout(G))
+    st.pyplot(plt)
 
-#Schedule to find links in database
+def in_list(val, val_list):
+    items = []
+    for item in val_list:
+        if (item > val-BUFF) and (item < val+BUFF) and (item != val):
+            items.append(item)
+    return items
+
+def makeNetwork():
+    global visits
+    global date
+    to = []
+    fro = []
+    if len(visits) > 1:
+        val_list = []
+        for val in visits.values():
+            for vis in val:
+                val_list.append(vis)
+        for val in val_list:
+            corresponding_values = in_list(val, val_list) #-1 for none or returns index, use index to find id, use id to connect IDs in to-fro list
+            if corresponding_values != []:
+                #Find id of corresponding value
+                ### Use [(i, colour.index(c)) for i, colour in enumerate(colours) if c in colour] to find index
+
+    else:
+        st.warning(f"There is only 1 person who has been detected since {date}.")
+        makeGraph([1], [1])
+
+st.set_page_config(page_title="Sentinare Contact Tracing",)
+st.title("Sentinare Disease Contact Tracing")
+user_id = st.sidebar.text_input('API Credential ID')
+user_secret = st.sidebar.text_input('API Secret')
+date = st.sidebar.date_input("Search from...", datetime.date.today())
+submit = st.sidebar.button('Submit')
+if submit:
+    if user_id and user_secret and date:
+        #if details check out --------------------------------------------------------
+        logVisits()
+        with st.spinner('Loading...'):
+            time.sleep(2)
+        fig = makeNetwork()
+        st.sidebar.success("Success!")
+    else:
+        st.sidebar.error("Fill all fields.")
+
+
+
+#Need to put entrance and exit
+#Can't restrict time in both directions
+#Hard to see face
